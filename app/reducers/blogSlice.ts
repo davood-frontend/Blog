8f6d542815f0f0e9802d0d1666b1fdaa93d2ca8c
David <davood.frontend@gmail.com>
2025-09-30 00:30:02 +0100

    feat: add blog slice

---
+++ app/reducers/blogSlice.ts
@@ -0,0 +1,10 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const blogsSlice = createSlice({
+    name :'blogs',
+    initialState: [],
+    reducers : {}
+})
+
+export default blogsSlice.reducer
