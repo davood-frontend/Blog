b59ad7ce23f16b9dc622c58745898477619fba45
David <davood.frontend@gmail.com>
2025-10-02 08:06:17 +0100

    test: add fake data
    add some fake blogs to test redux

--- app/reducers/blogSlice.ts
+++ app/reducers/blogSlice.ts
@@ -1,10 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
+const initialState = [
+    {
+        id: nanoid(),
+        date: new Date().toISOString(),
+        title: 'Very first blog',
+        content: 'blah blah blah, blah blah blah blah blah... blah blah',
+    },
+    {
+        id: nanoid(),
+        date: new Date().toISOString(),
+        title: 'Very second blog',
+        content: 'blah blah blah, blah blah blah blah blah... blah blah',
+    },
+    {
+        id: nanoid(),
+        date: new Date().toISOString(),
+        title: 'Very thirf blog',
+        content: 'blah blah blah, blah blah blah blah blah... blah blah',
+    },
+]
 
 const blogsSlice = createSlice({
-    name :'blogs',
-    initialState: [],
-    reducers : {}
+    name: 'blogs',
+    initialState: initialState,
+    reducers: {}
 })
 
 export default blogsSlice.reducer
