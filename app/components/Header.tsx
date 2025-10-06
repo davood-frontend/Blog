ffcb028984d1bfc88f9da9947cea21a0b1d6e48c
David <davood.frontend@gmail.com>
2025-10-06 01:15:21 +0100

    feat: add header

---
+++ app/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, AppBar, Toolbar, Button } from '@mui/material';
+const Header = () => {
+    return (
+        <Box>
+            <AppBar>
+                <Toolbar>
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Header;
