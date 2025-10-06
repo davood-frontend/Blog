234271b5e0344bd0aadccb62c671cdb75e5ea590
David <davood.frontend@gmail.com>
2025-10-06 23:20:51 +0100

    feat: add single blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box,Typography } from '@mui/material';
+const page = () => {
+    return (
+        <Box>
+            <Typography>
+                yaaaaaaaaah
+            </Typography>
+        </Box>
+    );
+};
+
+export default page;
--- app/components/BlogsList.tsx
+++ app/components/BlogsList.tsx
@@ -7,57 +7,54 @@ import { alpha } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Image from 'next/image';
 import EastRoundedIcon from '@mui/icons-material/EastRounded';
+import Link from 'next/link';
 const BlogsList = () => {
     const blogs = useSelector((state: RootState) => state.blogs)
     return (
         <Grid sx={{ p: 2 }} container spacing={3}>
             {blogs.map((item, index) => (
-                <Grid
-                    size={4}
-                    key={index}
-                    sx={{
-                        borderRadius: 4,
-                        overflow: 'hidden',
-                        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
-                        transition: '0.3s ease',
-                        '&:hover': {
-                            transform: 'translateY(-4px)',
-                            boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
-                        },
-                        '&:hover .card-button': {
-                            backgroundColor: (theme) => alpha(theme.palette.warning.main, 0.15),
-                            color: (theme) => theme.palette.warning.dark,
-                        },
-                    }}
-                >
-                    <Box sx={{ position: 'relative', width: '100%', height: 350 }}>
-                        <Image
-                            src="/blogSample.jpg"
-                            alt="blog cover"
-                            fill
-                            style={{ objectFit: 'cover' }}
-                            priority
-                        />
-                    </Box>
+                <Grid size={4} key={index} >
+                    <Link href='/blog'>
+                        <Box sx={{
+                            borderRadius: 4,
+                            overflow: 'hidden',
+                            boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                            transition: '0.3s ease',
+                            '&:hover': {
+                                transform: 'translateY(-4px)',
+                                boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+                            },
+                            '&:hover .card-button': {
+                                backgroundColor: (theme) => alpha(theme.palette.warning.main, 0.15),
+                                color: (theme) => theme.palette.warning.dark,
+                            }
+                        }}>
 
-                    <Box
-                        sx={{ p: 2, display: 'flex', alignItems: 'end', justifyContent: 'space-between', }}
-                    >
-                        <Box>
-                            <Typography variant="h5">{item.title}</Typography>
-                            <Typography>{item.content.substring(0, 50) + '...'}</Typography>
-                        </Box>
 
-                        <Box>
-                            <IconButton
-                                className="card-button"
-                                color="warning"
-                                sx={{ transition: 'background-color 0.5s ease, color 0.5s ease', }}
-                            >
-                                <EastRoundedIcon />
-                            </IconButton>
+                            <Box sx={{ position: 'relative', width: '100%', height: 350 }}>
+                                <Image
+                                    src="/blogSample.jpg"
+                                    alt="blog cover"
+                                    fill
+                                    style={{ objectFit: 'cover' }}
+                                    priority
+                                />
+                            </Box>
+
+                            <Box sx={{ p: 2, display: 'flex', alignItems: 'end', justifyContent: 'space-between', }} >
+                                <Box>
+                                    <Typography variant="h5">{item.title}</Typography>
+                                    <Typography>{item.content.substring(0, 50) + '...'}</Typography>
+                                </Box>
+
+                                <Box>
+                                    <IconButton className="card-button" color="warning" sx={{ transition: 'background-color 0.5s ease, color 0.5s ease', }}>
+                                        <EastRoundedIcon />
+                                    </IconButton>
+                                </Box>
+                            </Box>
                         </Box>
-                    </Box>
+                    </Link>
                 </Grid>
 
             ))}
