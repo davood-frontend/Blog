b3f7125eafa0fc1ca2760c30e9402f44e8ae9dd5
David <148909363+davood-frontend@users.noreply.github.com>
2025-10-06 01:20:53 +0100

    feat: add blogslist component

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,9 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import blogsReducer from '../reducers/blogSlice'
 
 
 export const store = configureStore({
-    reducer:{
+    reducer: {
         blogs: blogsReducer
     }
 })
+export type RootState = ReturnType<typeof store.getState>
+export type appDispatch = typeof store.dispatch
