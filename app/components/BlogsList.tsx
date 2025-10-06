32235417bef6552cf228cfcb7964f0d3f1ad02cd
David <davood.frontend@gmail.com>
2025-10-06 21:54:43 +0100

    style(blogs-list): improve cards style
    add box shadow and transition hover

--- app/components/BlogsList.tsx
+++ app/components/BlogsList.tsx
@@ -1,39 +1,65 @@
 'use client'
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, IconButton } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
+import { alpha } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Image from 'next/image';
-//background color black
-//golden vains 
-//dark theme in general
+import EastRoundedIcon from '@mui/icons-material/EastRounded';
 const BlogsList = () => {
     const blogs = useSelector((state: RootState) => state.blogs)
     return (
         <Grid sx={{ p: 2 }} container spacing={3}>
             {blogs.map((item, index) => (
-
-                <Grid key={index} sx={{ borderRadius: 4, overflow: 'hidden', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px;' }} size={4} >
-
+                <Grid
+                    size={4}
+                    key={index}
+                    sx={{
+                        borderRadius: 4,
+                        overflow: 'hidden',
+                        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                        transition: '0.3s ease',
+                        '&:hover': {
+                            transform: 'translateY(-4px)',
+                            boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+                        },
+                        '&:hover .card-button': {
+                            backgroundColor: (theme) => alpha(theme.palette.warning.main, 0.15),
+                            color: (theme) => theme.palette.warning.dark,
+                        },
+                    }}
+                >
                     <Box sx={{ position: 'relative', width: '100%', height: 350 }}>
                         <Image
                             src="/blogSample.jpg"
                             alt="blog cover"
                             fill
-                            objectFit='cover'
+                            style={{ objectFit: 'cover' }}
                             priority
                         />
                     </Box>
-                    <Box sx={{ p: 1.5 }}>
-                        <Typography variant='h5' >
-                            {item.title}
-                        </Typography>
-                        <Typography>
-                            {item.content}
-                        </Typography>
+
+                    <Box
+                        sx={{ p: 2, display: 'flex', alignItems: 'end', justifyContent: 'space-between', }}
+                    >
+                        <Box>
+                            <Typography variant="h5">{item.title}</Typography>
+                            <Typography>{item.content.substring(0, 50) + '...'}</Typography>
+                        </Box>
+
+                        <Box>
+                            <IconButton
+                                className="card-button"
+                                color="warning"
+                                sx={{ transition: 'background-color 0.5s ease, color 0.5s ease', }}
+                            >
+                                <EastRoundedIcon />
+                            </IconButton>
+                        </Box>
                     </Box>
                 </Grid>
+
             ))}
         </Grid>
     );
